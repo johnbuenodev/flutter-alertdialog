481daef24ca7e2c654601b5eca4ac2041808798e
John_bueno <stevewolfjp@hotmail.com>
2020-07-12 18:39:17 -0300

    add alertText, Alert icon,Alert Form com listview de Texformfield.

--- lib/main.dart
+++ lib/main.dart
@@ -21,12 +21,11 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
 class HomePage extends StatelessWidget {
-
-  final  _paddingbuttonStart = Padding(padding: EdgeInsets.fromLTRB(15.0, 0, 0, 0),);
-  final  _paddingButton = Padding(padding: EdgeInsets.only(left:20.0));
+  final _paddingbuttonStart = Padding(
+    padding: EdgeInsets.fromLTRB(15.0, 0, 0, 0),
+  );
+  final _paddingButton = Padding(padding: EdgeInsets.only(left: 20.0));
 
   @override
   Widget build(BuildContext context) {
@@ -52,54 +51,86 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.green,
       ),
       body: Container(
-          child: Center(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+    child: ListView(
+        children:<Widget>[
+          Center(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                //ListView(
+                  //children: <Widget>[
+                Center(
                  child: Column(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text("Exemplos de Alert!")
-                    ],
-        ),
-      ),
+                 children:<Widget>[
+
+                    Text("Exemplos de Alert!"),
+                    Padding(padding: EdgeInsets.only(top:20.0)),
+                    Text("Nome IOt:"),
+                    SizedBox(),
+                    Text("Identificação unica:"),
+                    SizedBox(width:30.0,),
+                    Text("Programação:"),
+                    SizedBox(width: 30.0,),
+                    Text("Setor Destino:"),
+                    SizedBox(width: 30.0,),
+                //  ],
+              //  ),
+                ],
+                ),
+                ),
+             ],
+            ),
           ),
+        ),
+      ],
+      ),
       ),
       floatingActionButton:
-      //_MenuFooter(context),
-      Row(
+          //_MenuFooter(context),
+          Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           _paddingbuttonStart,
           FloatingActionButton(
-              onPressed: (){},
+              onPressed: () {
+                _AlertText(context);
+              },
               backgroundColor: Colors.green,
               tooltip: "Textinfo",
-              child: Icon(Icons.text_fields)
-          ),
+              child: Icon(Icons.text_fields)),
           _paddingButton,
           FloatingActionButton(
-            onPressed: (){},
+            onPressed: () {
+
+
+            },
             backgroundColor: Colors.blueAccent,
             tooltip: "ImageInfo",
             child: Icon(Icons.image),
           ),
           _paddingButton,
           FloatingActionButton(
-            onPressed: (){},
+            onPressed: () {
+              _alertIcon(context);
+            },
             backgroundColor: Colors.deepOrange,
             tooltip: "IconInfo",
             child: Icon(Icons.insert_emoticon),
           ),
           _paddingButton,
           FloatingActionButton(
-              onPressed: (){},
+              onPressed: () {
+                _alertForm(context);
+              },
               backgroundColor: Colors.red,
               tooltip: "FormInfo",
-              child: Icon(Icons.format_align_left)
-          ),
+              child: Icon(Icons.format_align_left)),
         ],
       ),
-    );
+      );
+    //);
   }
 }
 
@@ -149,27 +180,191 @@ Widget _MenuFooter(BuildContext context) {
 }
 */
 
-Widget _AlertCloseApp(BuildContext context){
+Widget _AlertCloseApp(BuildContext context) {
   showDialog(
-      context: context,
-      builder: (BuildContext context){
-       return Container(
+    context: context,
+    builder: (BuildContext context) {
+      return Container(
         child: AlertDialog(
           title: Text("Deseja Sair?"),
           actions: <Widget>[
             FlatButton(
-                onPressed: (){
-                  Navigator.of(context).pop();
-                },
-                child: Text("Não"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Não"),
             ),
             FlatButton(
-                onPressed: () => exit(0),
-                child: Text("Sim"),
+              onPressed: () => exit(0),
+              child: Text("Sim"),
             ),
           ],
         ),
-       );
-      },
+      );
+    },
+  );
+}
+
+Widget _AlertText(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return Container(
+        child: AlertDialog(
+          title: Text(
+            "Nova Atualização do sistema! versão 2.0.1",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          content: Text(
+            "Deseja atualizar?",
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text(
+                "Não",
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text(
+                "Sim",
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+          elevation: 24.0,
+          backgroundColor: Colors.green,
+          //shape: CircleBorder(),
+        ),
+      );
+    },
+  );
+}
+
+Widget _alertIcon(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return Container(
+        child: AlertDialog(
+          title: Row(
+            children: <Widget>[
+              Icon(
+                Icons.insert_emoticon,
+                size: 50.0,
+                color: Colors.deepOrange,
+              ),
+              Text("Alert com Icon",style: TextStyle(color: Colors.black54),),
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                "Fechar",
+                textAlign: TextAlign.right,
+              ),
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
+
+Widget _alertForm(BuildContext context){
+  showDialog(
+    context: context,
+    builder: (BuildContext context){
+     return Container(
+       child: AlertDialog(
+         title: Row(
+           children: <Widget>[
+             Icon(
+                 Icons.device_hub,
+                 size:50.0,
+                 color: Colors.teal,
+             ),
+             Text(
+               "CADASTRAR IOT",
+               style: TextStyle(
+               color: Colors.indigo,
+             ),
+             ),
+           ],
+         ),
+         content: ListView(
+           children: <Widget>[
+           Form(
+             child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Nome:",
+                    labelStyle: TextStyle(color: Colors.indigo),
+                  ),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Identificação unica:",
+                    labelStyle: TextStyle(color: Colors.indigo),
+                  )
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Programação:",
+                    labelStyle: TextStyle(color: Colors.indigo),
+                  ),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText:  "Setor Destino:",
+                    labelStyle: TextStyle(
+                      color: Colors.indigo,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+         ),
+         ],
+         ),
+         actions: <Widget>[
+           FlatButton(
+               onPressed: (){
+                 Navigator.of(context).pop();
+               },
+               child: Text(
+                "Não",
+                style: TextStyle(
+                  color: Colors.black54,
+                ),
+               ),
+           ),
+           FlatButton(onPressed: (){
+
+           },
+           child: Text(
+             "Sim",
+             style: TextStyle(
+               color: Colors.black54,
+             ),
+           ),
+           ),
+         ],
+       ),
+     );
+    }
   );
 }
